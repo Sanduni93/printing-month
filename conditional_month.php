b9cebc1a56f171001ccfc1b991cc30c246ad5d88
Sanduni93 <nirodhajayaweera1993@gmail.com>
2018-06-15 08:29:04 +0530

    printing month by using both conditional and switch statements

---
+++ conditional_month.php
@@ -0,0 +1,60 @@
+<h2>Print name of month by using conditional statements..</h2>
+
+<?php
+
+$month = "Feb";
+
+if ($month=="Jan") {
+	echo "Month : January";
+}
+
+elseif ($month=="Feb") {
+	echo "Month : February";
+}
+
+elseif ($month=="Mar") {
+	echo "Month : March";
+}
+
+elseif ($month=="Apr") {
+	echo "Month : April";
+}
+
+elseif ($month=="May") {
+	echo "Month : May ";
+}
+
+elseif ($month=="June") {
+	echo "Month : June";
+}
+
+elseif ($month=="July") {
+	echo "Month : July";
+}
+
+elseif ($month=="Aug") {
+	echo "Month : August";
+}
+
+elseif ($month=="Sep") {
+	echo "Month : September";
+}
+
+elseif ($month=="Oct") {
+	echo "Month : Octomber";
+}
+
+elseif ($month=="Nov") {
+	echo "Month : November";
+}
+
+elseif ($month=="Dec") {
+	echo "Month : December";
+}
+
+else{
+
+	echo "Invalid month";
+}
+
+?>
---
+++ month.php
@@ -0,0 +1,52 @@
+<?php
+
+$month=date("m");
+
+switch ($month) {
+	               case '1':
+		echo "Jan";
+		break;
+		case '2':
+		echo "Feb";
+		break;
+		case '3':
+		echo "March";
+		break;
+		case '4':
+		echo "apr";
+		break;
+		case '5':
+		echo "may";
+		break;
+		case '6':
+		echo "june";
+		break;
+		case '7':
+		echo "july";
+		break;
+		case '8':
+		echo "aug";
+		break;
+		case '9':
+		echo "sep";
+		break;
+		case '10':
+		echo "oct";
+		break;
+		case '11':
+		echo "nove";
+		break;
+		case '12':
+		echo "dec";
+		break;
+	
+	default:
+		echo "invalid";
+			break;
+}
+
+
+
+
+
+?>
